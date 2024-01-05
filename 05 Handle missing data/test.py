72a8c4d58ba9f663bd0ebb116385b264fd90b63c
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-05 22:48:33 +0800

    test

---
+++ 05 Handle missing data/test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from icecream import ic 
+
+df = pd.read_csv('05 Handle missing data\weather_data.csv', parse_dates=['day'])
+df = df.set_index('day')
+
+dt = pd.date_range('2017-01-01', '2017-01-11')
+idx = pd.DatetimeIndex(dt)
+df = df.reindex(idx)
+
+df = round((df.interpolate(method='time')), 2)
+
+average_temperature = df.temperature.mean()
+print(average_temperature)
+
+# df.to_csv('new_data')
