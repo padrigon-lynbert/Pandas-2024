f5219c7c70f1c84fc8086e85d05d5c605730cfea
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-09 10:08:09 +0800

    replace missing/abnormal missing data

--- 06 Replace missing data/main6.py
+++ 06 Replace missing data/main6.py
@@ -4,15 +4,29 @@ import numpy as np
 
 df = pd.read_csv('06 Replace missing data/weather_data.csv')
 
-new_df = df.replace({
-    'temperature': -99999,
-    'windspeed': -99999,
-    'event': '0'
-}, np.NaN)
+# new_df = df.replace({
+#     'temperature': -99999,
+#     'windspeed': -99999,
+#     'event': '0'
+# }, np.NaN)
+
+# new_df = df.replace({
+#     -99999: np.NaN,
+#     '0': 'Sunny'
+# })
+# new_df  
+#.interpolate(method='linear')
+
+# with symbols
+# regex to replace non-numeric characters
 
 new_df = df.replace({
-    -99999: np.NaN,
-    '0': 'Sunny'
-})
-new_df  
-#.interpolate(method='linear') kjlnfg
+    'temperature': '[A-Za-z]',
+    'windspeed': '[A-Za-z]',
+}, '', regex=True)
+new_df
+
+
+#replace list values in a list of values
+
+df = pd.read_csv('06 Replace missing data/grades.csv').replace(['poor', 'average', 'good', 'exceptional'], [1,2,3,4])
