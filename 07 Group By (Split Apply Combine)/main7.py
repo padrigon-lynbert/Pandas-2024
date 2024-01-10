a1c8eaca455ff8cbe774c3c5e9cf5712bb4495e2
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-10 17:31:32 +0800

    split apply combine

--- 07 Group By (Split Apply Combine)/main7.py
+++ 07 Group By (Split Apply Combine)/main7.py
@@ -7,6 +7,34 @@ grouped = df.groupby('city')
 for city in grouped:
     city
 #find maximum temp in each of the cities
+import pandas as pd
+from icecream import ic
+
+df = pd.read_csv('07 Group By (Split Apply Combine)/weather_by_cities.csv')
+grouped = df.groupby('city')
+
+for city in grouped:
+    city
+
+#find maximum temp in each of the cities
+grouped.get_group('paris')
+#find average wind speed per city
+grouped.max()
+grouped.describe()
+
+import matplotlib.pyplot as plt
+
+grouped.plot()
+plt.show()
+
+for city, city_df in grouped:
+    city; city_df
+
+grouped.get_group('paris').plot()
+
+
+
+
 
 #find average wind speed per city
 
