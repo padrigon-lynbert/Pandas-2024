5dbd1aa6570cb1debb3b0d93e41d925ac16000df
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-11 10:50:45 +0800

    melt

--- 11  Reshape dataframe using melt/main11.py
+++ 11  Reshape dataframe using melt/main11.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_csv('11  Reshape dataframe using melt/weather.csv')
 df = pd.melt(df, id_vars=['day'], var_name="city", value_name='temperature')
 df
-df[df['variable']=='chicago', ]
+df[df['city']=='chicago']
