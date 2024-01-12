5b97293d26827892aeb70f0b23f24ad0619344e0
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-12 12:31:58 +0800

    stack/unstack (level)

---
+++ 12 Stack Unstack/main12.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_excel('12 Stack Unstack/stocks.xlsx', header=([0,1]))
+df_stacked = df.stack()
+df_stacked
+
+df = pd.read_excel('12 Stack Unstack/stocks_3_levels.xlsx', header=([0,1,2]))
+
+df
