9ee7e9e47918f69a1eab0a219454480fa004ede0
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-13 00:41:50 +0800

    datetime index, resample

---
+++ 14 datetimeIndex/main14.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('14 datetimeIndex/appl.csv', parse_dates=['Date'], index_col='Date')
+
+df.Close.mean()
+
+import matplotlib.pyplot as plt
+
+df.Close.resample('M').mean().plot()
+df.Close.resample('M').mean().plot(kind='density')
+
+plt.show()
+
